2300676e5cb153edd3396b387c3eff3f5b903b5e
Lobosanplay <parra.santiago2008@gmail.com>
2025-08-30 13:32:40 -0500

    agragar font-medium al botn de publicar post

--- app/admin/page.js
+++ app/admin/page.js
@@ -247,7 +247,7 @@ export default function AdminPanel() {
             
             <button
               onClick={createPost}
-              className="w-full px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors"
+              className="w-full px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors font-semibold"
             >
               Publicar Post
             </button>
